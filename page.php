bdb6d55e2aafae6d23e5d2047317548b4571c269
NotOnLand <NotOnLand@users.noreply.github.com>
2021-07-09 05:29:54 -0400

    Implemented parse_blocks

---
+++ page.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+
+</head>
+
+<body>
+
+  <?php get_template_part('nav') ?>
+
+  <?php get_sidebar(); ?>
+
+
+  <main id="pageContent" class="">
+      <article class="container-fluid px-0">
+
+
+            <?php
+              $post_id = get_the_ID();
+              $post = get_post($post_id);
+              $blocks = parse_blocks($post->post_content);
+
+              foreach ($blocks as $block) {
+                if(isset($block['blockName'])){
+                    echo '<section class="col-12 row mx-auto px-2"><div class="col-12">';
+                    echo render_block($block);
+                    echo '</div><div class="spacer d-none d-md-inline-block col-md-2"></div></section>';
+                  }
+                }
+              ?>
+
+
+      </article>
+  </main>
+
+<?php get_footer(); ?>
+</body>
+
+</html>
